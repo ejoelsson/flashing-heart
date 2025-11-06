3bbc005699bb9472043d6cc011531ba50f4268a0
Erik Joelsson <34108390+ejoelsson@users.noreply.github.com>
2025-11-06 18:25:10 +0100

    musik test

--- main.ts
+++ main.ts
@@ -1,5 +1,18 @@
-//  Added Documentation
-basic.forever(function on_forever() {
+input.onButtonPressed(Button.A, function () {
+    music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("C D E F G A B C5 ", 120), music.PlaybackMode.UntilDone)
+})
+input.onButtonPressed(Button.B, function () {
+    music.play(music.tonePlayable(196, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(196, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(233, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(196, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(175, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(156, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(147, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+})
+// Added Documentation
+basic.forever(function () {
     basic.showIcon(IconNames.Heart)
     basic.clearScreen()
     basic.pause(500)
